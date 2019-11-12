6f7740d023359531a19f6c82aa7531c32f09f109
volant360 <efrainh12@galileo.edu>
2019-11-12 17:04:56 -0600

    Db changes to support workflow

--- db/migrate/07020302_create_cloud_help_ticket_details.rb
+++ db/migrate/07020302_create_cloud_help_ticket_details.rb
@@ -7,8 +7,10 @@ class CreateCloudHelpTicketDetails < ActiveRecord::Migration[6.0]
             t.timestamps
         end
         add_reference :cloud_help_ticket_details, :cloud_help_ticket_types, foreign_key: true, index: { name: 'help_ticket_details_types' }
-        add_reference :cloud_help_ticket_details, :cloud_help_ticket_states, foreign_key: true, index: { name: 'help_ticket_details_states' }
+        add_reference :cloud_help_ticket_details, :cloud_help_ticket_state_assignments, foreign_key: true, index: { name: 'help_ticket_details_state_assignments' }
         add_reference :cloud_help_ticket_details, :cloud_help_ticket_priorities, foreign_key: true, index: { name: 'help_ticket_details_priorities' }
+        add_reference :cloud_help_ticket_details, :cloud_help_ticket_sources, foreign_key: true, index: { name: 'help_ticket_details_sources' }
+        add_reference :cloud_help_ticket_details, :cloud_help_ticket_categories, foreign_key: true, index: { name: 'help_ticket_details_categories' }
         add_reference :cloud_help_ticket_details, :cloud_help_tickets, foreign_key: true
     end
 end
