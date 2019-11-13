3a0cc4b1bab770c8c12652d7cbed15763cfc993c
volant360 <efrainh12@galileo.edu>
2019-11-13 08:59:32 -0600

    Removed migration sla_details and added name to slas migration

--- db/migrate/07020202_create_cloud_help_sla_details.rb
+++
@@ -1,8 +0,0 @@
-class CreateCloudHelpSlaDetails < ActiveRecord::Migration[6.0]
-    def change
-        create_table :cloud_help_sla_details do |t|
-            t.timestamps
-        end
-        add_reference :cloud_help_sla_details, :cloud_help_slas, foreign_key: true
-    end
-end
