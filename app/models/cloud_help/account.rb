aa25e50f1316585fda8f99517ab43a01e635054f
volant360 <efrainh12@galileo.edu>
2019-11-29 07:38:18 -0600

    Naive Followers implementation

--- app/models/cloud_help/account.rb
+++ app/models/cloud_help/account.rb
@@ -1,6 +1,6 @@
 module CloudHelp
     class Account < ApplicationRecord
-        belongs_to :account, class_name: "::Account", foreign_key: "id"
+        belongs_to :account, class_name: "Account", foreign_key: "id"
         has_many :tickets, foreign_key: 'cloud_help_accounts_id'
         has_many :ticket_categories, foreign_key: 'cloud_help_accounts_id'
         has_many :ticket_priorities, foreign_key: 'cloud_help_accounts_id'
