8f41aad9f11aadc4715aa2a6102d1c14c513d529
volant360 <efrainh12@galileo.edu>
2019-12-18 10:41:59 -0600

    Quick Actions moved to core

--- app/models/cloud_help/ticket/action.rb
+++ app/models/cloud_help/ticket/action.rb
@@ -1,5 +1,5 @@
 module CloudHelp
-    class Ticket::Action < ApplicationRecord
-        belongs_to :ticket, class_name: "CloudHelp::Ticket", foreign_key: 'cloud_help_tickets_id'
+    class Ticket::Action < CloudObject::Action
+        belongs_to :cloud_object, class_name: "CloudHelp::Ticket", foreign_key: 'cloud_help_tickets_id'
     end
 end
