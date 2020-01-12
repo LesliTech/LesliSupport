7fb82b413818b4462512c9e9404419c91c4e7489
volant360 <efrainh12@galileo.edu>
2020-01-12 12:09:11 -0600

    add trello/037 Documentation for ticket detail model

--- app/models/cloud_help/ticket/detail.rb
+++ app/models/cloud_help/ticket/detail.rb
@@ -1,4 +1,30 @@
 module CloudHelp
+=begin
+
+Lesli
+
+Copyright (c) 2020, Lesli Technologies, S. A.
+
+All the information provided by this website is protected by laws of Guatemala related 
+to industrial property, intellectual property, copyright and relative international laws. 
+Lesli Technologies, S. A. is the exclusive owner of all intellectual or industrial property
+rights of the code, texts, trade mark, design, pictures and any other information.
+Without the written permission of Lesli Technologies, S. A., any replication, modification,
+transmission, publication is strictly forbidden.
+For more information read the license file including with this software.
+
+LesliCloud - Your Smart Business Assistant
+
+Powered by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@author   Carlos Hermosilla
+@license  Propietary - all rights reserved.
+@version  0.1.0-alpha
+@description Model for ticket detail. Each *ticket* has only one detail, that contains all relevant
+    information (like *category*, *priority*, *source*, etc.). This is to maintaing the *cloud_help_tickets* table small. 
+    The detail is not accessed directly, rather, it is accessed only through its *ticket*
+=end
     class Ticket::Detail < ApplicationRecord
         include ActiveModel::Dirty
         
