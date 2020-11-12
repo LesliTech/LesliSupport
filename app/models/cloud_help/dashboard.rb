774971573b270ff1b018812656a2beed104d8bfa
volant360 <efrainh12@galileo.edu>
2020-11-12 13:40:06 -0600

    add trello/1986 default dashboard creation when account is initialized

--- app/models/cloud_help/dashboard.rb
+++ app/models/cloud_help/dashboard.rb
@@ -4,5 +4,77 @@ module CloudHelp
         
         has_many :components, inverse_of: :dashboard, autosave: true, class_name: "Dashboard::Component", foreign_key: "cloud_help_dashboards_id", dependent: :destroy
         accepts_nested_attributes_for :components, allow_destroy: true
+
+        def self.initialize_data(account)
+            self.create!(
+                account: account,
+                name: "Default Dashboard",
+                default: true,
+                main: false,
+                components_attributes: [
+                    {
+                        name: "Latest Tickets",
+                        component_id: "new_tickets",
+                        layout: 4,
+                        index: 2,
+                        query_configuration: {
+                            filters: {},
+                            pagination: {per_page: 6}
+                        },
+                        custom_configuration: {
+                            arrangement: {}
+                        }
+                    },{
+                        name: "My Tickets",
+                        component_id: "my_tickets",
+                        layout: 4,
+                        index: 3,
+                        query_configuration: {
+                            filters: {},
+                            pagination: {per_page: 6}
+                        },
+                        custom_configuration: {
+                            arrangement: {}
+                        }
+                    },{
+                        name: "Unassigned Tickets",
+                        component_id: "unassigned_tickets",
+                        layout: 4,
+                        index: 4,
+                        query_configuration: {
+                            filters: {},
+                            pagination: {per_page: 6}
+                        },
+                        custom_configuration: {
+                            arrangement: {}
+                        }
+                    },{
+                        name: "Tickets by Type",
+                        component_id: "tickets_by_type",
+                        layout: 6,
+                        index: 0,
+                        query_configuration: {
+                            filters: {},
+                            pagination: {}
+                        },
+                        custom_configuration: {
+                            arrangement: {}
+                        }
+                    },{
+                        name: "Tickets by Category",
+                        component_id: "tickets_by_category",
+                        layout: 6,
+                        index: 0,
+                        query_configuration: {
+                            filters: {},
+                            pagination: {}
+                        },
+                        custom_configuration: {
+                            arrangement: {}
+                        }
+                    }
+                ]
+            )
+        end
     end
 end
