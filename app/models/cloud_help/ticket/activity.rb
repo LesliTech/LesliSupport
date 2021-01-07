542017fb01a5e361293452f667178cb06ae70bae
volant360 <efrainh12@galileo.edu>
2021-01-07 12:28:47 -0600

    fix: trello/2108 belongs_to user association fixed for activities

--- app/models/cloud_help/ticket/activity.rb
+++ app/models/cloud_help/ticket/activity.rb
@@ -19,7 +19,6 @@ For more information read the license file including with this software.
 =end
     class Ticket::Activity < CloudObject::Activity
         belongs_to :cloud_object, class_name: "CloudHelp::Ticket", foreign_key: "cloud_help_tickets_id", optional: true
-        belongs_to :user, class_name: "::User", foreign_key: "users_id", optional: true
 
         enum category: {
             action_list:                    "action_list",
