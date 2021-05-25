81e8903fc51ff3143065f6dfc3c7ee04c1f9f88e
volant360 <efrainh12@galileo.edu>
2021-05-25 19:45:49 +0200

    fix: Fixed subscriber model for sla

--- app/models/cloud_help/sla/subscriber.rb
+++ app/models/cloud_help/sla/subscriber.rb
@@ -1,4 +1,5 @@
 module CloudHelp
-  class Sla::Subscriber < ApplicationRecord
+  class Sla::Subscriber < CloudObject::Subscriber
+    belongs_to :cloud_object, class_name: "CloudHelp::Sla", foreign_key: "cloud_help_slas_id"
   end
 end
