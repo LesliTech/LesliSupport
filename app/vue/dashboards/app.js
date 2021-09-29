34452d2b542720ea36dc66c834352b275a0295da
diego-alay <dalay@lomax.com.gt>
2021-09-28 20:38:12 -0600

    feat: upload js files

--- app/vue/dashboards/app.js
+++ app/vue/dashboards/app.js
@@ -1,9 +1,9 @@
 /*
 Copyright (c) 2020, all rights reserved.
 
-All the information provided by this platform is protected by international laws related  to 
-industrial property, intellectual property, copyright and relative international laws. 
-All intellectual or industrial property rights of the code, texts, trade mark, design, 
+All the information provided by this platform is protected by international laws related  to
+industrial property, intellectual property, copyright and relative international laws.
+All intellectual or industrial property rights of the code, texts, trade mark, design,
 pictures and any other information belongs to the owner of this platform.
 
 Without the written permission of the owner, any replication, modification,
@@ -18,7 +18,7 @@ For more information read the license file including with this software.
 
 // · Import main app
 // · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
-import app from "LesliVue/app"
+import app from "LesliCoreVue/app"
 
 // · Import apps and components
 // · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
--- lib/cloud_help/version.rb
+++ lib/cloud_help/version.rb
@@ -1,4 +1,4 @@
 module CloudHelp
   VERSION = '0.0.27'
-    BUILD = "20210928.1349"
+    BUILD = "20210928.2033"
 end
