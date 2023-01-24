695c99f121f22c6ec10f912a563a389bdd0cfcc3
FranciscoRosal <ros18676@uvg.edu.gt>
2023-01-24 07:56:08 -0600

    chore: update code

--- app/controllers/cloud_help/ticket/timelines_controller.rb
+++ app/controllers/cloud_help/ticket/timelines_controller.rb
@@ -3,11 +3,11 @@ require_dependency "cloud_help/application_controller"
 module CloudHelp
 =begin
 
-Copyright (c) 2020, all rights reserved.
+Copyright (c) 2023, all rights reserved.
 
-All the information provided by this platform is protected by international laws related  to 
-industrial property, intellectual property, copyright and relative international laws. 
-All intellectual or industrial property rights of the code, texts, trade mark, design, 
+All the information provided by this platform is protected by international laws related  to
+industrial property, intellectual property, copyright and relative international laws.
+All intellectual or industrial property rights of the code, texts, trade mark, design,
 pictures and any other information belongs to the owner of this platform.
 
 Without the written permission of the owner, any replication, modification,
@@ -16,25 +16,25 @@ transmission, publication is strictly forbidden.
 For more information read the license file including with this software.
 
 // · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
-// · 
+// ·
 
 =end
     class Ticket::TimelinesController < ApplicationLesliController
 
 =begin
 @return [JSON] Json that contains a list of all events in the ticket timeline
-@description Retrieves and returns all timeline events associated to a *CloudHelp::Ticket*. 
+@description Retrieves and returns all timeline events associated to a *CloudHelp::Ticket*.
     The timeline is also filtered by *account* and *current_user* is also used.
 @example
     # Executing this controller's action from javascript's frontend
     this.http.get(`127.0.0.1/help/ticket_priorities`);
 =end
         def index
-            responseWithSuccessful(Ticket::Timeline.index(current_user, @query, params[:ticket_id]))
+            respond_with_successful(Ticket::Timeline.index(current_user, @query, params[:ticket_id]))
         end
 
         def options
-            responseWithSuccessful(Ticket::Timeline.options(current_user, @query))
+            respond_with_successful(Ticket::Timeline.options(current_user, @query))
         end
     end
 end
