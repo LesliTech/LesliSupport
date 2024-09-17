8886139624e87ffa6ef5751a030d7e5e0e7ed8e5
Luis Donis <ldonis.emc2@gmail.com>
2024-09-16 21:10:43 -0600

    feat: implement table component

--- lib/lesli_support/version.rb
+++ lib/lesli_support/version.rb
@@ -1,4 +1,4 @@
 module LesliSupport
   VERSION = "0.1.0"
-  BUILD = "1717901903"
+  BUILD = "1719784526"
 end
---
+++ lib/vue/apps/tickets/components/table.vue
@@ -0,0 +1,94 @@
+<script setup>
+/*
+Lesli
+
+Copyright (c) 2023, Lesli Technologies, S. A.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+
+Lesli · Ruby on Rails SaaS Development Framework.
+
+Made with ♥ by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@contact  hello@lesli.tech
+@website  https://www.lesli.tech
+@license  GPLv3 http://www.gnu.org/licenses/gpl-3.0.en.html
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+*/
+
+
+// · import vue tools
+import { useRouter } from "vue-router"
+
+
+// · import lesli stores
+import { useTickets } from "LesliSupport/vue/stores/tickets"
+
+
+// · initialize/inject plugins
+const url = inject("url")
+
+
+// · implement stores
+const storeTickets = useTickets()
+
+
+// · translations
+const translations = {
+    tickets: i18n.t("lesli_support.tickets"),
+    lesli: i18n.t("lesli")
+}
+
+
+// ·
+const columns = [{
+    field: "id",
+    label: translations.tickets.column_id,
+    sort: true
+}, {
+    field: "subject",
+    label: translations.tickets.column_subject,
+    sort: true
+}, {
+    field: "deadline",
+    label: translations.tickets.column_deadline,
+    sort: true
+}, {
+    field: "status_name",
+    label: translations.tickets.column_status,
+    align: "center",
+    sort: true
+},{
+    field: "priority_name",
+    label: translations.tickets.column_priority,
+    align: "center",
+    sort: true
+}, {
+    field: "user_name",
+    label: translations.tickets.column_creator,
+    sort: true
+}]
+</script>
+<template>
+    <lesli-table 
+        :columns="columns"
+        :records="storeTickets.index.records" 
+        :loading="storeTickets.index.loading"
+        :pagination="storeTickets.index.pagination"
+        :link="(ticket) => url.support('tickets/:id', ticket.id)">
+    </lesli-table>
+</template>
